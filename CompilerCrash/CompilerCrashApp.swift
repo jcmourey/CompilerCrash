b6134e74bd182410f7b7d88b394980d34eaa9b73
jcmourey <jean-charles@mourey.com>
2024-07-01 19:10:31 +0200

    @ObservationStateIgnored prevents synthesizing Equatable conformance for State. Removing Equatable makes the compiler crash!

--- CompilerCrash/CompilerCrashApp.swift
+++ CompilerCrash/CompilerCrashApp.swift
@@ -1,12 +1,39 @@
-//
-
 import SwiftUI
+import ComposableArchitecture
+
+@Reducer
+struct CompilerCrash {
+    @Reducer(state: .equatable)
+    public enum Destination {
+        case signIn(SignIn)
+    }
+    @ObservableState
+    struct State: Equatable {
+        @Presents var destination: Destination.State?
+    }
+}
+
+actor SignInService {}
+
+@Reducer
+struct SignIn {
+    @ObservableState
+    struct State: Equatable {   // removing Equatable makes the compiler crash
+        let userName: String
+        
+        @ObservationStateIgnored
+        let signInService: SignInService
+        
+// Workaround:
+//        static func == (lhs: Self, rhs: Self) -> Bool {
+//            lhs.userName == rhs.userName
+//        }
+    }
+}
 
 @main
 struct CompilerCrashApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup {}
     }
 }
